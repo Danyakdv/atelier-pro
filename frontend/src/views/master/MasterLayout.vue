ac8d6c2b90f7fb43325fa956a40ab8305c1d60d4
xl <danil.danilka2003@list.ru>
2026-03-22 19:01:18 +0300

    Delete frontend directory

--- frontend/src/views/master/MasterLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page-wrapper">
-    <SidebarNav />
-    <main class="main-content"><router-view /></main>
-  </div>
-</template>
-<script setup>
-import SidebarNav from '@/components/shared/SidebarNav.vue'
-</script>
